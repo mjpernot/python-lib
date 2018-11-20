f80cff557a0862fb8041c7f5b26b4b2f0e37ce33
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2018-11-20 10:50:22 +0000

    Add test/unit/gen_class/Mail_add_2_msg.py v2.7.0 - Unit test of gen_class.Mail.add_2_msg method

---
+++ test/unit/gen_class/Mail_add_2_msg.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  Mail_add_2_msg.py
+
+    Description:  Unit testing of Mail.add_2_msg in gen_class.py.
+
+    Usage:
+        test/unit/gen_class/Mail_add_2_msg.py
+
+    Arguments:
+        None
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import gen_class
+import version
+
+# Version
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:  None
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_initial_add -> Test with adding data to empty message.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+            None
+
+        """
+
+        self.to = ["mail_address@domain.name"]
+
+    def test_initial_add(self):
+
+        """Function:  test_initial_add
+
+        Description:  Test with adding data to empty message.
+
+        Arguments:
+            None
+
+        """
+
+        msg = "Test email line"
+
+        email = gen_class.Mail(self.to)
+
+        email.add_2_msg(msg)
+
+        self.assertEqual((email.to, email.msg), (self.to, msg))
+
+
+if __name__ == "__main__":
+    unittest.main()
