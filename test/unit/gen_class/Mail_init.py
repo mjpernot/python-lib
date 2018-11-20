77d7cd2daca0b1c86847800bb4fe13cc40122293
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2018-11-20 10:08:02 +0000

    Update test/unit/gen_class/Mail_init.py v2.7.0 - Added test_to_list test

--- test/unit/gen_class/Mail_init.py
+++ test/unit/gen_class/Mail_init.py
@@ -47,6 +47,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_to_list -> Test to line with a list.
         test_default -> Test with minimum number of arguments.
 
     """
@@ -67,6 +68,22 @@ class UnitTest(unittest.TestCase):
         self.frm = None
         self.host_name = None
         self.host = None
+
+    def test_to_list(self):
+
+        """Function:  test_to_list
+
+        Description:  Test to line with a list.
+
+        Arguments:
+            None
+
+        """
+
+        email = gen_class.Mail(self.to)
+
+        self.assertEqual((email.to, email.subj, email.frm, email.host_name,
+                          email.host), (self.to, None, None, None, None))
 
     def test_default(self):
 
