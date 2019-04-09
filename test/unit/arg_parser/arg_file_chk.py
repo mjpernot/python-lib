4c6a1d520408698afa178d93c089f99bf546583a
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-09 08:14:53 +0100

    Update test/unit/arg_parser/arg_file_chk.py

--- test/unit/arg_parser/arg_file_chk.py
+++ test/unit/arg_parser/arg_file_chk.py
@@ -76,6 +76,10 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Unit testing initilization.
+        test_name_loop_zero_items -> Test with name loop on zero items.
+        test_name_loop_two_items -> Test with name loop on two items.
+        test_name_loop_one_item -> Test with name loop on one item.
+        test_isinstance_is_set -> Test with isinstance against a set.
         test_isinstance_is_string -> Test with isinstance against a string.
         test_isinstance_is_list -> Test with isinstance against a list.
         test_two_match_between_sets -> Test with two matches between sets.
@@ -99,6 +103,53 @@ class UnitTest(unittest.TestCase):
         self.file_chk_list = ["-f"]
         self.args_array = {"-f": ["test/file1"], "-m": "Marker"}
         self.open = FileOpen()
+
+    @mock.patch("arg_parser.open")
+    def test_name_loop_zero_items(self, mock_open):
+
+        """Function:  test_name_loop_zero_items
+
+        Description:  Test with name loop on zero items.
+
+        Arguments:
+            None
+
+        """
+
+        self.args_array = {"-f": [], "-m": "Marker"}
+
+        self.assertFalse(arg_parser.arg_file_chk(self.args_array,
+                                                 self.file_chk_list))
+
+    @mock.patch("arg_parser.open")
+    def test_name_loop_two_items(self, mock_open):
+
+        """Function:  test_name_loop_two_items
+
+        Description:  Test with name loop on two items.
+
+        Arguments:
+            None
+
+        """
+
+        self.assertFalse(arg_parser.arg_file_chk(self.args_array,
+                                                 self.file_chk_list))
+
+    @mock.patch("arg_parser.open")
+    def test_name_loop_one_item(self, mock_open):
+
+        """Function:  test_name_loop_one_item
+
+        Description:  Test with name loop on one item.
+
+        Arguments:
+            None
+
+        """
+
+        self.assertFalse(arg_parser.arg_file_chk(self.args_array,
+                                                 self.file_chk_list))
 
     @mock.patch("arg_parser.open")
     def test_isinstance_is_set(self, mock_open):
