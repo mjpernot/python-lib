62328eff935a4214352300324b69ec104d439bac
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-03 09:47:01 +0000

    Update arg_cond_req_or.py

--- test/unit/arg_parser/arg_cond_req_or.py
+++ test/unit/arg_parser/arg_cond_req_or.py
@@ -49,10 +49,11 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Unit testing initilization.
-        test_two_args_missing -> Test with two arguments required, but missing.
-        test_one_arg_missing -> Test with one argument required, but missing.
-        test_two_args_required -> Test with 2 arguments required & are present.
-
+        test_multi_cond_req2 -> Test multi conditional requirements, 1 missing.
+        test_multi_cond_req -> Test multiple conditional requirements present.
+        test_two_cond_req2 -> Test two conditional requirements, one missing.
+        test_two_cond_req -> Test with two conditional requirements.
+        test_two_args_present -> Test with two arguments are present.
         test_one_arg_present -> Test with one argument is present.
         test_empty_optconreqdict -> Test with empty dict for opt_con_req_dict.
         test_empty_argsarray -> Test with empty args_array.
@@ -76,18 +77,19 @@ class UnitTest(unittest.TestCase):
         self.args_array4 = {"-c": "config", "-f": "file1", "-m": "file2"}
         self.args_array5 = {"-c": "config", "-d": "dir", "-f": "file1",
                             "-m": "file2"}
+        self.args_array6 = {"-c": "config", "-d": "dir", "-f": "file1"}
 
         self.opt_con_req_dict = {}
-        self.opt_con_req_dict2 = {"-c": ["-m"]}
+        self.opt_con_req_dict2 = {"-c": ["-f"]}
         self.opt_con_req_dict3 = {"-c": ["-f", "-m"]}
         self.opt_con_req_dict4 = {"-c": ["-f"] , "-f": ["-m"]}
         self.opt_con_req_dict5 = {"-c": ["-f", "-m"] , "-d": ["-c"]}
 
-    def test_two_args_missing(self):
+    def test_multi_cond_req2(self):
 
-        """Function:  test_two_args_missing
+        """Function:  test_multi_cond_req2
 
-        Description:  Test with two arguments required, but missing.
+        Description:  Test with multiple conditional requirements, one missing.
 
         Arguments:
             None
@@ -95,41 +97,69 @@ class UnitTest(unittest.TestCase):
         """
 
         with gen_libs.no_std_out():
-            status = arg_parser.arg_cond_req_or(self.args_array2,
-                                             self.opt_con_req_dict4)
+            status = arg_parser.arg_cond_req_or(self.args_array6,
+                                                self.opt_con_req_dict5)
 
         self.assertFalse(status)
 
-    def test_one_arg_missing(self):
+    def test_multi_cond_req(self):
 
-        """Function:  test_one_arg_missing
+        """Function:  test_multi_cond_req
 
-        Description:  Test with one argument required, but missing.
+        Description:  Test with multiple conditional requirements present.
 
         Arguments:
             None
 
         """
         
-        with gen_libs.no_std_out():
-            status = arg_parser.arg_cond_req_or(self.args_array2,
-                                             self.opt_con_req_dict3)
+        self.assertTrue(arg_parser.arg_cond_req_or(self.args_array5,
+                                                   self.opt_con_req_dict5)
 
-        self.assertFalse(status)
+    def test_two_cond_req2(self):
 
-    def test_two_args_required(self):
+        """Function:  test_two_cond_req2
 
-        """Function:  test_two_args_required
-
-        Description:  Test with two arguments required and are present.
+        Description:  Test with two conditional requirements, one missing.
 
         Arguments:
             None
 
         """
 
-        self.assertTrue(arg_parser.arg_cond_req_or(self.args_array3,
-                                                self.opt_con_req_dict3))
+        with gen_libs.no_std_out():
+            status = arg_parser.arg_cond_req_or(self.args_array3,
+                                                self.opt_con_req_dict4)
+
+        self.assertFalse(status)
+
+    def test_two_cond_req(self):
+
+        """Function:  test_two_cond_req
+
+        Description:  Test with two conditional requirements.
+
+        Arguments:
+            None
+
+        """
+        
+        self.assertTrue(arg_parser.arg_cond_req_or(self.args_array4,
+                                                   self.opt_con_req_dict4)
+
+    def test_two_args_present(self):
+
+        """Function:  test_two_args_present
+
+        Description:  Test with two arguments are present.
+
+        Arguments:
+            None
+
+        """
+
+        self.assertTrue(arg_parser.arg_cond_req_or(self.args_array4,
+                                                   self.opt_con_req_dict3))
 
     def test_one_arg_present(self):
 
