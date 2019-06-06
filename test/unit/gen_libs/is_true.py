f6fa461eacea6b5e4abcce1381c9a149cd3e7e4d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-06-06 15:51:59 +0100

    Add test/unit/gen_libs/is_true.py

---
+++ test/unit/gen_libs/is_true.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  is_true.py
+
+    Description:  Unit testing of is_true in gen_libs.py.
+
+    Usage:
+        test/unit/gen_libs/is_true.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+from __future__ import print_function
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Unit testing initilization.
+        test_is_true_off -> Test with False value.
+        test_is_true_on -> Test with True value.
+        test_is_true_no -> Test with No value.
+        test_is_true_yes -> Test with Yes value.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.x1 = "Yes"
+        self.x2 = "No"
+        self.x3 = "ON"
+        self.x4 = "OFF"
+
+    def test_is_true_off(self):
+
+        """Function:  test_is_true_off
+
+        Description:  Test with OFF value.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(gen_libs.is_true(self.x4), False)
+
+    def test_is_true_on(self):
+
+        """Function:  test_is_true_on
+
+        Description:  Test with ON value.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(gen_libs.is_true(self.x3), True)
+
+    def test_is_true_no(self):
+
+        """Function:  test_is_true_no
+
+        Description:  Test with No value.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(gen_libs.is_true(self.x2), False)
+
+    def test_is_true_yes(self):
+
+        """Function:  test_is_true_yes
+
+        Description:  Test with Yes value.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(gen_libs.is_true(self.x1), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
