9eb1358e8cecc98d23fbb4f0ebf5ac95f3695c34
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-09-17 12:57:18 +0100

    Documentation update for cmds_gen unit test files

--- test/unit/cmds_gen/add_cmd.py
+++ test/unit/cmds_gen/add_cmd.py
@@ -40,10 +40,6 @@ class UnitTest(unittest.TestCase):
 
     Description:  Class which is a representation of a unit testing.
 
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
-
     Methods:
         setUp -> Unit testing initilization.
         test_cmd_value -> Test with command and value.
