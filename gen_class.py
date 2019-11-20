0fccc190b4eb69257edf2d2b9b06ac09c17b15fc
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-20 16:59:58 +0000

    Update gen_class.LogFile.load_keyword v2.7.7 - Updated method to append data to current keyword list

--- gen_class.py
+++ gen_class.py
@@ -466,12 +466,12 @@ class LogFile(object):
 
         elif isinstance(data, list):
             data = list(data)
-            self.keyword = self.ignore.extend([x.lower().rstrip().rstrip("\n")
-                                               for x in data])
+            self.keyword.extend([x.lower().rstrip().rstrip("\n")
+                                 for x in data])
 
         elif isinstance(data, str):
-            self.keyword = \
-                         data.lower().rstrip().rstrip("\n").split(fld_delimit)
+            self.keyword.append(
+                data.lower().rstrip().rstrip("\n").split(fld_delimit))
 
     def load_loglist(self, data, dictkey=None, **kwargs):
 
