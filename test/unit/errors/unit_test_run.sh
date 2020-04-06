7144b8d5fda9fdf8079f6817b63ea95c0e7ea3c8
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-06 11:39:09 +0100

    Add test/unit/errors/unit_test_run.sh v2.8.1

---
+++ test/unit/errors/unit_test_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Unit testing program for the errors.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file 
+#   is located at.
+
+echo ""
+echo "Unit test"
+test/unit/errors/EmptyRowError.py
+test/unit/errors/Errors.py
+test/unit/errors/NoOptionError.py
+test/unit/errors/NotMasterError.py
+test/unit/errors/NotSlaveError.py
+test/unit/errors/NotYetImplementedError.py
+test/unit/errors/SlaveNotRunningError.py
+
