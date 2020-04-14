9772d68c400cab18f47111ceef041f6a064caa06
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-14 10:01:49 +0100

    Update test/integration/gen_libs/integration_test_run.sh v2.8.2

--- test/integration/gen_libs/integration_test_run.sh
+++ test/integration/gen_libs/integration_test_run.sh
@@ -9,7 +9,7 @@ echo "Unit test:  gen_libs.py"
 test/integration/gen_libs/chk_crt_dir.py
 test/integration/gen_libs/chk_crt_file.py
 test/integration/gen_libs/dict_2_std.py
-#test/integration/gen_libs/dir_file_match.py
+test/integration/gen_libs/dir_file_match.py
 #test/integration/gen_libs/display_data.py
 #test/integration/gen_libs/filename_search.py
 #test/integration/gen_libs/make_md5_hash.py
