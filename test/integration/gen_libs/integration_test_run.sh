e5c26a93833d1f70403e3fcf94dafcdb970592f8
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-14 11:05:15 +0100

    Update test/integration/gen_libs/integration_test_run.sh v2.8.2

--- test/integration/gen_libs/integration_test_run.sh
+++ test/integration/gen_libs/integration_test_run.sh
@@ -14,7 +14,7 @@ test/integration/gen_libs/display_data.py
 test/integration/gen_libs/filename_search.py
 test/integration/gen_libs/make_md5_hash.py
 test/integration/gen_libs/merge_data_types.py
-#test/integration/gen_libs/print_dict.py
+test/integration/gen_libs/print_dict.py
 #test/integration/gen_libs/rotate_files.py
 #test/integration/gen_libs/write_to_log.py
 
