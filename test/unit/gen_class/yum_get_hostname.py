3c1fb7b34a2234c9b4358c893a4d2aa772ecf036
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-13 10:37:45 +0100

    Updated test/unit/gen_class/yum_get_hostname.py v2.8.2

--- test/unit/gen_class/yum_get_hostname.py
+++ test/unit/gen_class/yum_get_hostname.py
@@ -57,7 +57,7 @@ class UnitTest(unittest.TestCase):
         """
 
         self.host_name = "HOSTNAME"
-        self.os = "Linux"
+        self.osys = "Linux"
         self.release = "2.6"
         self.distro = ("Centos", "7.5.1804", "Core")
 
