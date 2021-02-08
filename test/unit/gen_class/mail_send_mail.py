4ebe5c82ae8ca540e8a50e9786567221d4a53283
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-02-08 10:46:57 +0000

    Updated test/unit/gen_class/mail_send_mail.py v2.8.3

--- test/unit/gen_class/mail_send_mail.py
+++ test/unit/gen_class/mail_send_mail.py
@@ -34,6 +34,63 @@ import version
 __version__ = version.__version__
 
 
+class SubProcess(object):
+
+    """Class:  SubProcess
+
+    Description:  Class which is a representation of the subprocess class.
+
+    Methods:
+        __init__ -> Initialization instance.
+        Popen -> Mock representation of subprocess.Popen method.
+        wait -> Mock representation of subprocess.wait method.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Initialization instance.
+
+        Arguments:
+
+        """
+
+        self.PIPE = "Pipe Redirect"
+        self.stdout = None
+        self.stdin = None
+        self.cmd = None
+
+    def Popen(self, cmd, stdout=None, stdin=None):
+
+        """Method:  Popen
+
+        Description:  Mock representation of subprocess.Popen method.
+
+        Arguments:
+
+        """
+
+        self.stdout = stdout
+        self.stdin = stdin
+        self.cmd = cmd
+
+        return "Proc Instance"
+
+    def wait(self):
+
+        """Method:  wait
+
+        Description:  Mock representation of subprocess.wait method.
+
+        Arguments:
+
+        """
+
+        pass
+
+
 class Smtplib(object):
 
     """Class:  SubProcess
@@ -99,6 +156,14 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Unit testing initilization.
+        test_to_list2 -> Test to address as a list.
+        test_to_list -> Test to address as a list.
+        test_to_str -> Test to address as a string.
+        test_subj_spaces2 -> Test subject line with white spaces.
+        test_subj_spaces -> Test subject line with white spaces.
+        test_subj_str -> Test subject line as a string.
+        test_send_mailx2 -> Test send_mailx function.
+        test_send_mailx -> Test send_mailx function.
         test_send_mail -> Test test_send_mail function.
 
     """
@@ -115,6 +180,158 @@ class UnitTest(unittest.TestCase):
 
         self.frm = "From_Address"
         self.toaddr = "To_Address"
+        self.toaddr2 = ["To_Address"]
+        self.toaddr3 = ["To_Address", "To_Address2"]
+        self.toaddr3a = "To_Address To_Address2"
+        self.subj = "SubjectLine"
+        self.subj2 = ["SubjectLine2"]
+        self.subj2a = "SubjectLine2"
+        self.subj3 = ["Subject", "Line3"]
+        self.subj3a = "SubjectLine3"
+        self.subp = SubProcess()
+        self.subp2 = SubProcess()
+
+    @mock.patch("gen_class.subprocess")
+    def test_to_list2(self, mock_subp):
+
+        """Function:  test_to_list2
+
+        Description:  Test to address as a list.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr3, subj=self.subj, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj, self.toaddr3a))
+
+    @mock.patch("gen_class.subprocess")
+    def test_to_list(self, mock_subp):
+
+        """Function:  test_to_list
+
+        Description:  Test to address as a list.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr2, subj=self.subj, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj, self.toaddr))
+
+    @mock.patch("gen_class.subprocess")
+    def test_to_str(self, mock_subp):
+
+        """Function:  test_to_str
+
+        Description:  Test to address as a string.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr, subj=self.subj, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj, self.toaddr))
+
+    @mock.patch("gen_class.subprocess")
+    def test_subj_spaces2(self, mock_subp):
+
+        """Function:  test_subj_spaces2
+
+        Description:  Test subject line with white spaces.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr, subj=self.subj3, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj3a, self.toaddr))
+
+    @mock.patch("gen_class.subprocess")
+    def test_subj_spaces(self, mock_subp):
+
+        """Function:  test_subj_spaces
+
+        Description:  Test subject line with white spaces.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr, subj=self.subj2, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj2a, self.toaddr))
+
+    @mock.patch("gen_class.subprocess")
+    def test_subj_str(self, mock_subp):
+
+        """Function:  test_subj_str
+
+        Description:  Test subject line as a string.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr, subj=self.subj, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj, self.toaddr))
+
+    @mock.patch("gen_class.subprocess")
+    def test_send_mailx2(self, mock_subp):
+
+        """Function:  test_send_mailx2
+
+        Description:  Test send_mailx function.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr, subj=self.subj, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        mail.send_mail(use_mailx=True)
+
+        self.assertEqual((mail.subj, mail.to), (self.subj, self.toaddr))
+
+    @mock.patch("gen_class.subprocess")
+    def test_send_mailx(self, mock_subp):
+
+        """Function:  test_send_mailx
+
+        Description:  Test send_mailx function.
+
+        Arguments:
+
+        """
+
+        mail = gen_class.Mail(self.toaddr, subj=self.subj, frm=self.frm)
+        mock_subp.side_effect = [self.subp, self.subp2]
+
+        self.assertFalse(mail.send_mail(use_mailx=True))
 
     @mock.patch("gen_class.Mail.create_body")
     @mock.patch("gen_class.smtplib.SMTP")
