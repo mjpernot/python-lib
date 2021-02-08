b048afeeebb47570ee4349e946ede980eae5cf02
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-02-08 10:17:53 +0000

    Updated test/unit/sonarqube_code_coverage.sh v2.8.3

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -165,6 +165,7 @@ coverage run -a --source=gen_class test/unit/gen_class/mail_create_subject.py
 coverage run -a --source=gen_class test/unit/gen_class/mail_print_email.py
 coverage run -a --source=gen_class test/unit/gen_class/mail_read_stdin.py
 coverage run -a --source=gen_class test/unit/gen_class/mail_send_mail.py
+coverage run -a --source=gen_class test/unit/gen_class/mail_send_mailx.py
 coverage run -a --source=machine test/unit/machine/linux.py
 coverage run -a --source=machine test/unit/machine/amachine.py
 coverage run -a --source=machine test/unit/machine/solaris.py
