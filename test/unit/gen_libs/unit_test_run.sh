892ff72344e9e1814e029107351d93d229268925
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-02-08 12:07:36 +0000

    Updated test/unit/gen_libs/unit_test_run.sh v2.8.3

--- test/unit/gen_libs/unit_test_run.sh
+++ test/unit/gen_libs/unit_test_run.sh
@@ -73,6 +73,7 @@ test/unit/gen_libs/rename_file.py
 test/unit/gen_libs/rm_dup_list.py
 test/unit/gen_libs/rm_file.py
 test/unit/gen_libs/rm_newline_list.py
+test/unit/gen_libs/rm_whitespace.py
 test/unit/gen_libs/root_run.py
 test/unit/gen_libs/rotate_files.py
 test/unit/gen_libs/str_2_list.py
