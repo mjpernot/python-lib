af3431f4234689925ab40d42b347626eddf027a6
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-09-01 15:55:13 +0100

    Updated unit test files v2.8.4

--- test/unit/gen_class/logfile_init.py
+++ test/unit/gen_class/logfile_init.py
@@ -40,8 +40,8 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialize testing environment.
-        test_default -> Test with minimum number of arguments.
+        setUp
+        test_default
 
     """
 
