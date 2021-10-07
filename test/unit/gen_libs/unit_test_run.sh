070ea8b34d3a0ef90d6711bb9e0c37e92ca4d2c5
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-10-07 16:24:39 +0100

    Update test/unit/gen_libs/unit_test_run.sh v2.8.5

--- test/unit/gen_libs/unit_test_run.sh
+++ test/unit/gen_libs/unit_test_run.sh
@@ -68,6 +68,7 @@ test/unit/gen_libs/normalize.py
 test/unit/gen_libs/not_in_list.py
 test/unit/gen_libs/no_std_out.py
 test/unit/gen_libs/openfile.py
+test/unit/gen_libs/pascalize.py
 test/unit/gen_libs/pct_int.py
 test/unit/gen_libs/perm_check.py
 test/unit/gen_libs/print_data.py
