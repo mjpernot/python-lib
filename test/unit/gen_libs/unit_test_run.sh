41dacd36cf189067669031c7e21fd30fd4bf616d
Mark Pernot <mark.pernot@gmail.com>
2021-10-12 11:14:29 +0100

    Update test/unit/gen_libs/unit_test_run.sh v2.8.5

--- test/unit/gen_libs/unit_test_run.sh
+++ test/unit/gen_libs/unit_test_run.sh
@@ -32,6 +32,7 @@ test/unit/gen_libs/file_cleanup.py
 test/unit/gen_libs/file_search_cnt.py
 test/unit/gen_libs/file_search.py
 test/unit/gen_libs/filename_search.py
+test/unit/gen_libs/find_email_addr.py
 test/unit/gen_libs/float_div.py
 test/unit/gen_libs/get_base_dir.py
 test/unit/gen_libs/get_data.py
