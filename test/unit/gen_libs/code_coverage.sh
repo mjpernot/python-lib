8f7eed231e1bb1273003a9b9b9a341b2bf41c585
Mark Pernot <mark.pernot@gmail.com>
2021-12-27 09:39:53 +0000

    Update test/unit/gen_libs/code_coverage.sh v2.8.6

--- test/unit/gen_libs/code_coverage.sh
+++ test/unit/gen_libs/code_coverage.sh
@@ -70,6 +70,7 @@ coverage run -a --source=gen_libs test/unit/gen_libs/mv_file2.py
 coverage run -a --source=gen_libs test/unit/gen_libs/normalize.py
 coverage run -a --source=gen_libs test/unit/gen_libs/not_in_list.py
 coverage run -a --source=gen_libs test/unit/gen_libs/no_std_out.py
+coverage run -a --source=gen_libs test/unit/gen_libs/octal_to_str.py
 coverage run -a --source=gen_libs test/unit/gen_libs/openfile.py
 coverage run -a --source=gen_libs test/unit/gen_libs/pascalize.py
 coverage run -a --source=gen_libs test/unit/gen_libs/pct_int.py
