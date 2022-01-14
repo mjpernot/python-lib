86e3f6e9042988e7fad8d96a74c8be657db62fb4
Mark Pernot <mark.pernot@gmail.com>
2022-01-14 09:06:20 +0000

    Update test/unit/sonarqube_code_coverage.sh v2.9.0

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -138,6 +138,7 @@ coverage run -a --source=gen_class test/unit/gen_class/argparser_arg_cond_req_or
 coverage run -a --source=gen_class test/unit/gen_class/argparser_arg_default.py
 coverage run -a --source=gen_class test/unit/gen_class/argparser_arg_dir_chk.py
 coverage run -a --source=gen_class test/unit/gen_class/argparser_arg_dir_chk_crt.py
+coverage run -a --source=gen_class test/unit/gen_class/argparser_arg_file_chk.py
 coverage run -a --source=gen_class test/unit/gen_class/argparser_init.py
 coverage run -a --source=gen_class test/unit/gen_class/daemon_delpid.py
 coverage run -a --source=gen_class test/unit/gen_class/daemon_init.py
