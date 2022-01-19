eb4b5f04a43847bef394095151c97d84064ae3a9
Mark Pernot <mark.pernot@gmail.com>
2022-01-19 10:46:10 +0000

    Update test/unit/gen_class/unit_test_run.sh v2.9.0

--- test/unit/gen_class/unit_test_run.sh
+++ test/unit/gen_class/unit_test_run.sh
@@ -17,6 +17,7 @@ test/unit/gen_class/argparser_arg_file_chk.py
 test/unit/gen_class/argparser_arg_noreq_xor.py
 test/unit/gen_class/argparser_arg_parse2.py
 test/unit/gen_class/argparser_arg_require.py
+test/unit/gen_class/argparser_arg_req_or_lst.py
 test/unit/gen_class/argparser_init.py
 test/unit/gen_class/daemon_delpid.py
 test/unit/gen_class/daemon_init.py
