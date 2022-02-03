070322c379781462222c473cdf762f1928f3dc30
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-02-03 16:06:05 +0000

    Update test/unit/gen_class/unit_test_run.sh v2.9.0

--- test/unit/gen_class/unit_test_run.sh
+++ test/unit/gen_class/unit_test_run.sh
@@ -13,6 +13,7 @@ test/unit/gen_class/argparser_arg_cond_req_or.py
 test/unit/gen_class/argparser_arg_default.py
 test/unit/gen_class/argparser_arg_dir_chk.py
 test/unit/gen_class/argparser_arg_dir_chk_crt.py
+test/unit/gen_class/argparser_arg_dir_crt.py
 test/unit/gen_class/argparser_arg_file_chk.py
 test/unit/gen_class/argparser_arg_noreq_xor.py
 test/unit/gen_class/argparser_arg_parse2.py
