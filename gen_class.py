d2b8b3e44e80b86681081580e924c95b703ebac9
Mark Pernot <mark.pernot@gmail.com>
2022-09-15 10:53:23 +0100

    Update gen_class.py v2.9.4

--- gen_class.py
+++ gen_class.py
@@ -52,7 +52,7 @@ import gzip
 import json
 import re
 
-# yum==3.4.3 does not work in Python3
+# yum==3.4.3 does not work in Python 3
 if sys.version_info < (3, 0):
     import yum
 
@@ -2425,144 +2425,144 @@ class Logger(object):
             self.log.removeHandler(handle)
 
 # The package yum==3.4.3 only works with Python 2.7
-#if sys.version_info < (3, 0):
-class Yum(yum.YumBase):
+if sys.version_info < (3, 0):
+    class Yum(yum.YumBase):
 
-    """Class:  Yum
+        """Class:  Yum
 
-    Description:  Class which is a representation for YumBase system class.  A
-        yum object is used as a proxy for using the yum command.
+        Description:  Class which is a representation for YumBase system class.  A
+            yum object is used as a proxy for using the yum command.
 
-    Methods:
-        __init__
-        get_hostname
-        get_os
-        get_release
-        get_distro
-        fetch_repos
-        fetch_install_pkgs
-        fetch_update_pkgs
-
-    """
-
-    def __init__(self, host_name=None):
-
-        """Method:  __init__
-
-        Description:  Initialization of an instance of the Yum class.
-
-        Arguments:
-            (input) host_name -> Host name of server
+        Methods:
+            __init__
+            get_hostname
+            get_os
+            get_release
+            get_distro
+            fetch_repos
+            fetch_install_pkgs
+            fetch_update_pkgs
 
         """
 
-        yum.YumBase.__init__(self)
+        def __init__(self, host_name=None):
 
-        if host_name:
-            self.host_name = host_name
+            """Method:  __init__
 
-        else:
-            self.host_name = socket.gethostname()
+            Description:  Initialization of an instance of the Yum class.
 
-        self.os_name = platform.system()
-        self.release = platform.release()
-        self.distro = platform.linux_distribution()
+            Arguments:
+                (input) host_name -> Host name of server
 
-    def get_distro(self):
+            """
 
-        """Method:  get_distro
+            yum.YumBase.__init__(self)
 
-        Description:  Reuturn class' linux_distribution.
+            if host_name:
+                self.host_name = host_name
 
-        Arguments:
-            (output) self.distro -> Linux distribution tuple value
+            else:
+                self.host_name = socket.gethostname()
 
-        """
+            self.os_name = platform.system()
+            self.release = platform.release()
+            self.distro = platform.linux_distribution()
 
-        return self.distro
+        def get_distro(self):
 
-    def get_hostname(self):
+            """Method:  get_distro
 
-        """Method:  get_hostname
+            Description:  Reuturn class' linux_distribution.
 
-        Description:  Return the class' hostname.
+            Arguments:
+                (output) self.distro -> Linux distribution tuple value
 
-        Arguments:
-            (output) self.host_name -> Server's host name
+            """
 
-        """
+            return self.distro
 
-        return self.host_name
+        def get_hostname(self):
 
-    def get_os(self):
+            """Method:  get_hostname
 
-        """Method:  get_os
+            Description:  Return the class' hostname.
 
-        Description:  Return the class' OS platform
+            Arguments:
+                (output) self.host_name -> Server's host name
 
-        Arguments:
-            (output) self.os_name -> Server's Operating system name.
+            """
 
-        """
+            return self.host_name
 
-        return self.os_name
+        def get_os(self):
 
-    def get_release(self):
+            """Method:  get_os
 
-        """Method:  get_release
+            Description:  Return the class' OS platform
 
-        Description:  Return the class' OS release version.
+            Arguments:
+                (output) self.os_name -> Server's Operating system name.
 
-        Arguments:
-            (output) self.release -> Kernel release version
+            """
 
-        """
+            return self.os_name
 
-        return self.release
+        def get_release(self):
 
-    def fetch_repos(self):
+            """Method:  get_release
 
-        """Method:  fetch_repos
+            Description:  Return the class' OS release version.
 
-        Description:  Return a list of repos.
+            Arguments:
+                (output) self.release -> Kernel release version
 
-        Arguments:
-            (output) List of repositories
+            """
 
-        """
+            return self.release
 
-        self.doRepoSetup()
+        def fetch_repos(self):
 
-        return [repo.name for repo in self.repos.listEnabled()]
+            """Method:  fetch_repos
 
-    def fetch_install_pkgs(self):
+            Description:  Return a list of repos.
 
-        """Method:  fetch_install_pkgs
+            Arguments:
+                (output) List of repositories
 
-        Description:  Return a dictionary of installed packages in a list.
+            """
 
-        Arguments:
-            (output) List of installed of packages in JSON format
+            self.doRepoSetup()
 
-        """
+            return [repo.name for repo in self.repos.listEnabled()]
 
-        return [{"package": pkg.name, "ver": pkg.version, "arch": pkg.arch}
-                for pkg in self.rpmdb]
+        def fetch_install_pkgs(self):
 
-    def fetch_update_pkgs(self):
+            """Method:  fetch_install_pkgs
 
-        """Method:  fetch_update_pkgs
+            Description:  Return a dictionary of installed packages in a list.
 
-        Description:  Return a dictionary of packages to be updated in a
-            list.
+            Arguments:
+                (output) List of installed of packages in JSON format
 
-        Arguments:
-            (output) List of packages to be installed/updated in JSON format
+            """
 
-        """
+            return [{"package": pkg.name, "ver": pkg.version, "arch": pkg.arch}
+                    for pkg in self.rpmdb]
 
-        return [{"package": pkg.name, "ver": pkg.version, "arch": pkg.arch,
-                 "repo": str(getattr(pkg, "repo"))}
-                for pkg in self.doPackageLists(pkgnarrow="updates",
-                                               patterns="",
-                                               ignore_case=True)]
+        def fetch_update_pkgs(self):
+
+            """Method:  fetch_update_pkgs
+
+            Description:  Return a dictionary of packages to be updated in a
+                list.
+
+            Arguments:
+                (output) List of packages to be installed/updated in JSON format
+
+            """
+
+            return [{"package": pkg.name, "ver": pkg.version, "arch": pkg.arch,
+                     "repo": str(getattr(pkg, "repo"))}
+                    for pkg in self.doPackageLists(pkgnarrow="updates",
+                                                   patterns="",
+                                                   ignore_case=True)]
