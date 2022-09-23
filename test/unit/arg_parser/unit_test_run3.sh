b89309d0bb0912c898587bc78d44b432aba0623c
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-09-23 16:16:05 +0100

    Merge of mod/294c1 into mod/294

--- test/unit/arg_parser/unit_test_run3.sh
+++ test/unit/arg_parser/unit_test_run3.sh
@@ -6,25 +6,25 @@
 
 echo ""
 echo "Unit test:"
-python3 test/unit/arg_parser/_make_dir.py
-python3 test/unit/arg_parser/arg_add_def.py
-python3 test/unit/arg_parser/arg_cond_req.py
-python3 test/unit/arg_parser/arg_cond_req_or.py
-python3 test/unit/arg_parser/arg_default.py
-python3 test/unit/arg_parser/arg_dir_chk.py
-python3 test/unit/arg_parser/arg_dir_chk_crt.py
-python3 test/unit/arg_parser/arg_file_chk.py
-python3 test/unit/arg_parser/arg_noreq_xor.py
-python3 test/unit/arg_parser/arg_parse2.py
-python3 test/unit/arg_parser/arg_require.py
-python3 test/unit/arg_parser/arg_req_or_lst.py
-python3 test/unit/arg_parser/arg_req_xor.py
-python3 test/unit/arg_parser/arg_set_path.py
-python3 test/unit/arg_parser/arg_validate.py
-python3 test/unit/arg_parser/arg_valid_val.py
-python3 test/unit/arg_parser/arg_wildcard.py
-python3 test/unit/arg_parser/arg_xor_dict.py
-python3 test/unit/arg_parser/parse_multi.py
-python3 test/unit/arg_parser/parse_single.py
-python3 test/unit/arg_parser/file_create.py
+/usr/bin/python3 ./test/unit/arg_parser/_make_dir.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_add_def.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_cond_req.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_cond_req_or.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_default.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_dir_chk.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_dir_chk_crt.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_file_chk.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_noreq_xor.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_parse2.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_require.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_req_or_lst.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_req_xor.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_set_path.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_validate.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_valid_val.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_wildcard.py
+/usr/bin/python3 ./test/unit/arg_parser/arg_xor_dict.py
+/usr/bin/python3 ./test/unit/arg_parser/parse_multi.py
+/usr/bin/python3 ./test/unit/arg_parser/parse_single.py
+/usr/bin/python3 ./test/unit/arg_parser/file_create.py
 
